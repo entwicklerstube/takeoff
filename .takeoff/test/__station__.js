952220e6c0fc1ed5e6e806f361e5c3dc445f93af
michaelzoidl <github@michaelzoidl.com>
2017-05-29 21:08:08 +0200

    remove not used stuff

--- .takeoff/test/__station__.js
+++
@@ -1,9 +0,0 @@
-import testTemplate from './test.js'
-
-export default {
-  requiredProps: ['name'],
-  config: props => {
-    path: `lib/test/__${props.name}__.js`,
-    template: testTemplate
-  }
-}
