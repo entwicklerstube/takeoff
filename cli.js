01c9eed5fa583ae06869227d1967cfb0b36baf74
michaelzoidl <github@michaelzoidl.com>
2017-05-29 20:56:48 +0200

    update cli

--- cli.js
+++ cli.js
@@ -1,36 +1,25 @@
 #!/usr/bin/env node
-const Listr = require('listr');
 const inquirer = require('inquirer')
-const fs = require('fs')
-const exec = require('child_process').exec;
 
 const { getOfficialStations } = require('./lib/station')
 
-getOfficialStations().then(() => {
-  console.log('hey');
-})
+const TakeOff = async () => {
+  const officialStations = await getOfficialStations()
 
-fs.readdir(__dirname + '/stations', (err, files) => {
-  const b = files.filter(file => !/(.DS_Store|_shared)/.test(file))
-
-  var questions = [{
+  const takeOffSteps = [{
     type: 'list',
     name: 'station',
     message: 'Choose your station',
     choices: []
-    .concat(b)
-    // if .takeoff folder found in project
-    .concat([new inquirer.Separator(`Found custom stations in "${process.cwd().split('/').pop()}"`)])
-    .concat(['A', 'B']),
-    filter: val => val.toLowerCase()
+      .concat(officialStations)
   }]
 
-  inquirer.prompt(questions).then(function (answers) {
-    console.log('\nOrder receipt:');
-    console.log(JSON.stringify(answers));
-    });
-})
+  const answers = await inquirer.prompt(takeOffSteps)
 
+  console.log('-------', answers);
+}
+
+TakeOff()
 
 
 
