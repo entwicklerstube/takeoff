46ef08c4f596f6c02b80921060204e9c83d24c80
michaelzoidl <github@michaelzoidl.com>
2017-05-29 23:05:02 +0200

    update tests

--- test.js
+++ test.js
@@ -1,94 +1,103 @@
-const mock = require('mock-fs');
-const expect = require('chai').expect
+const path = require('path');
+const mockFs = require('mock-fs');
+const mockRequire = require('mock-require');
+const expect = require('chai').expect;
 
-const { getPredefinedStations, getCustomStations, loadStationByName } = require('./lib/station')
+const {getPredefinedStations, getCustomStations, loadStationByName} = require('./lib/station');
 
 describe('lib/station', () => {
   describe('getPredefinedStations', () => {
     beforeEach(() => {
-      mock({
+      mockFs({
         'stations/node-module': {},
         'stations/component': {}
       });
-    })
+    });
 
     afterEach(() => {
-      mock.restore()
-    })
+      mockFs.restore();
+    });
 
     it('returns array with available stations', async () => {
-      const officialStations = await getPredefinedStations()
-      expect(officialStations).to.be.a('array')
-    })
+      const officialStations = await getPredefinedStations();
+      expect(officialStations).to.be.a('array');
+    });
 
     it('returns mocked station', async () => {
-      const officialStations = await getPredefinedStations()
-      expect(officialStations).to.deep.equal(['component', 'node-module'])
-    })
+      const officialStations = await getPredefinedStations();
+      expect(officialStations).to.deep.equal(['component', 'node-module']);
+    });
 
     it('returns empty array if no mocked stations are available', async () => {
-      mock.restore()
+      mockFs.restore();
 
-      mock({
-        'stations': {}
+      mockFs({
+        stations: {}
       });
 
-      const officialStations = await getPredefinedStations()
-      expect(officialStations).to.deep.equal([])
-    })
-  })
+      const officialStations = await getPredefinedStations();
+      expect(officialStations).to.deep.equal([]);
+    });
+  });
 
   describe('getCustomStations', () => {
     beforeEach(() => {
-      mock({
+      mockFs({
         '.takeoff/custom-station': {}
       });
-    })
+    });
 
     afterEach(() => {
-      mock.restore()
-    })
+      mockFs.restore();
+    });
 
     it('returns array', async () => {
-      const customStations = await getCustomStations()
-      expect(customStations).to.be.a('array')
-    })
+      const customStations = await getCustomStations();
+      expect(customStations).to.be.a('array');
+    });
 
     it('returns array with custom stations (in a .takeoff folder)', async () => {
-      const customStations = await getCustomStations()
-      expect(customStations).to.deep.equal(['custom-station'])
-    })
+      const customStations = await getCustomStations();
+      expect(customStations).to.deep.equal(['custom-station']);
+    });
 
     it('returns empty array if .takeoff folder doesnt exist ', async () => {
-      mock.restore()
+      mockFs.restore();
 
-      const customStations = await getCustomStations()
-      expect(customStations).to.deep.equal([])
-    })
-  })
+      const customStations = await getCustomStations();
+      expect(customStations).to.deep.equal([]);
+    });
+  });
 
   describe('loadStationByName', () => {
-    it('returns a object', async () => {
-      const station = await loadStationByName()
-      expect(station).to.be.a('object')
-    })
+    beforeEach(() => {
+      mockFs({
+        '.takeoff/custom-station': {}
+      });
+
+      const mockRequireCustomStationPath = path.resolve(__dirname, '.takeoff', 'custom-station', '__station__.js');
+      mockRequire(mockRequireCustomStationPath, {mocked: 'station'});
+    });
+
+    afterEach(() => {
+      mockFs.restore();
+    });
 
     it('returns a object', async () => {
-      const station = await loadStationByName()
-      expect(station).to.be.a('object')
-    })
-  })
-})
+      const station = await loadStationByName('custom-station');
+      expect(station).to.be.a('object');
+    });
 
-
-
-
-
-
-
-
-
-
-
+    // It('returns a object', async () => {
+    //   const station = await loadStationByName();
+    //   expect(station).to.be.a('object');
+    // });
+    //
+    // it('returns a object', async () => {
+    //   const station = await loadStationByName();
+    //   expect(station).to.be.a('object');
+    // });
+  });
+});
 
 //
