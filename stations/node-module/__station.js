7a61b1521cf12d55fc0a530b13dde00999ba7175
michaelzoidl <github@michaelzoidl.com>
2017-05-30 18:49:11 +0200

    update node-module station

--- stations/node-module/__station.js
+++ stations/node-module/__station.js
@@ -1,10 +1,52 @@
+const {spawn} = require('child_process');
 const license = require('../_shared/license/mit.js');
 const gitignore = require('./gitignore.js');
 const readme = require('./readme.md.js');
 const packageJson = require('./package.json.js');
+const travisYml = require('./travis.yml.js');
+const testJs = require('./test.js');
+const indexJs = require('./index.js');
+
+const requiredNodePackages = ['mocha', 'chai', 'nyc'];
 
 module.exports = {
-  requiredProps: ['name', 'email'],
+  postTakeoff: (props, {emit, succeed}) => {
+    emit('Install packages');
+
+    const ls = spawn('yarn', ['add'].concat(requiredNodePackages));
+
+    ls.stdout.on('data', data => {
+      emit(data.toString().trim().split('\n').pop());
+    });
+
+    ls.on('exit', succeed);
+  },
+  requiredProps: [{
+    type: 'input',
+    name: 'packagename',
+    message: 'How do you want to call your node-module?',
+    default: process.cwd().split('/').pop()
+  }, {
+    type: 'input',
+    name: 'username',
+    message: 'Whats your name?',
+    default: process.env.USER
+  }, {
+    type: 'input',
+    name: 'user_email',
+    message: 'Whats your email?'
+  }, {
+    type: 'list',
+    name: 'license',
+    message: 'What license do you want?',
+    choices: [{
+      name: 'MIT',
+      value: 'mit'
+    }, {
+      name: 'Apache License 2.0',
+      value: 'apache-license-2.0'
+    }]
+  }],
   run: props => ({
     files: [{
       filename: 'package.json',
@@ -18,6 +60,15 @@ module.exports = {
     }, {
       filename: 'LICENSE',
       template: license(props)
+    }, {
+      filename: 'index.js',
+      template: indexJs(props)
+    }, {
+      filename: '.travis.yml',
+      template: travisYml()
+    }, {
+      filename: 'test.js',
+      template: testJs(props)
     }]
-  })
-};
+  }
+)};
