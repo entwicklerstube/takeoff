fcd6e7dd268d7a996089945c6c973d23d22e3820
michaelzoidl <github@michaelzoidl.com>
2018-10-20 00:55:49 +0200

    update indexjs handling

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 import command from "commander";
 import { version } from "./package.json";
 
-import getAvailableStations from "./lib/discover-stations";
+import findStations from "./lib/find-local-stations";
 
 (async () => {
   command
     .version(version)
+    .command("takeoff <station>")
     .option(
       "--import",
       "Import / convert an existing file or hirachy into a station."
     )
-    .option("-c, --config", "Define a additional resource for stations.")
+    .option("--init", "Initialize takeoff in the current folder")
     .parse(process.argv);
 
-  if (command.import) {
-    console.log("OIDA IMPORT");
-    return;
+  switch (true) {
+    case command.import:
+      console.log("IMPORT");
+      break;
+
+    case command.init:
+      console.log("INIT");
+      break;
+
+    default:
+      const localStations = await findStations();
+      console.log(localStations);
   }
-
-  // remote station
-
-  const availableStations = await getAvailableStations();
-
-  // findStations
-
-  // discover available stations
 })();
