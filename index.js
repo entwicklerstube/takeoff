c8b95cde1d4e7e506727ffcf619a8b6f33eb1a9e
michaelzoidl <github@michaelzoidl.com>
2018-10-25 08:16:32 +0200

    update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import command from "commander";
 import { version } from "./package.json";
 
 import findStations from "./lib/find-local-stations";
+// import stationsToInquirer from "./lib/stations-to-inquirer";
 
 (async () => {
   command
@@ -15,16 +16,42 @@ import findStations from "./lib/find-local-stations";
     .parse(process.argv);
 
   switch (true) {
-    case command.import:
-      console.log("IMPORT");
+    case command.import: {
+      console.log(
+        "IMPORT, pass path of a file/folder and we will move it to the stationPath (can also be passed)"
+      );
       break;
+    }
 
-    case command.init:
-      console.log("INIT");
+    case command.init: {
+      console.log(
+        "INIT, creates a .takeoff folder within a easy example station"
+      );
       break;
+    }
 
-    default:
+    default: {
       const localStations = await findStations();
       console.log(localStations);
+      // stationsToInquirer();
+
+      /*
+      // The CLI Interface relays on the awesome API of inquirer, this module converts the station-data into the CLI prompts
+      // Execute this if a user runs takeoff without arguments
+      const { station, props } = await stationsToInquirer(localStations);
+
+      // Now we know what station we want to execute with what properties, try it! Show a indicator
+      console.log(
+        "we want to execute the station with the props",
+        executeStation
+      );
+
+      // Resolve the exec, this works also if the exec is not a promise
+      await Promise.resolve(station.exec.bind(props));
+
+      process.stdout.write('Yay, we`re done');
+
+      */
+    }
   }
 })();
