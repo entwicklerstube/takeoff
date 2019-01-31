948891ac95828ec6508d6bbae876c12bb2789cef
Michael Zoidl <github@michaelzoidl.com>
2019-01-31 19:53:40 +0100

    removed old takeoff stations in favor of using the new stations in config

--- .takeoff/lib/index-template.js
+++
@@ -1,8 +0,0 @@
-const pascalCase = require("change-case").pascalCase;
-
-module.exports = props => `const ${pascalCase(props.name)} = () => {
-  return true;
-};
-
-export default ${pascalCase(props.name)};
-`;
