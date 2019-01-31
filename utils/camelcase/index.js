ae8fee189f19680d4fdfff4cf31ebecae8a048b6
Michael Zoidl <github@michaelzoidl.com>
2019-01-31 19:54:31 +0100

    add comments to *case utils

--- utils/camelcase/index.js
+++ utils/camelcase/index.js
@@ -1,5 +1,13 @@
-import { camel } from "change-case";
+import { camel } from 'change-case';
 
+/**
+ * Convert the case-type of a string to camelCase
+ * @import `import { camelCase } from 'takeoff/utils')`
+ * @example
+ * const camelCase = require('takeoff/utils');
+ * camelCase('hello-world')
+ * // => helloWorld
+ */
 export const Camelcase = camel;
 
 export default Camelcase;
--- utils/paramcase/index.js
+++ utils/paramcase/index.js
@@ -1,5 +1,12 @@
-import { paramCase } from "change-case";
+import { paramCase } from 'change-case';
 
+/**
+ * Convert the case-type of a string to paramCase
+ * @import `import { paramCase } from 'takeoff/utils'`
+ * @example
+ * paramCase('hello-world')
+ * // => hello-world
+ */
 export const ParamCase = paramCase;
 
 export default ParamCase;
