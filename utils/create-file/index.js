8fea29e35abf0dc229763fc630a7d5c094d74373
Michael Zoidl <github@michaelzoidl.com>
2019-02-28 14:58:09 +0100

    update create-file util

--- utils/create-file/index.js
+++ utils/create-file/index.js
@@ -1,14 +1,13 @@
 import { mkdir } from 'fs';
-import { path } from 'change-case';
 
 /**
- * Convert the case-type of a string to paramCase
+ * Create a foder
  * @import `import { createFolder } from 'takeoff/utils'`
  * @example
  * await createFolder('hello-world')
  * // => successfully created the folder 'hello-world' in the "__dirname" folder
  */
-export const createFolder = async ({ path, options = {} }) => {
+export const createFolder = async (path, options = {}) => {
   mkdir(path, options, err => {
     if (err) {
       throw err;
--- utils/create-file/test.js
+++ utils/create-file/test.js
@@ -1,7 +1,21 @@
 import createFolder from './';
+import fs from 'fs';
+import { promisify } from 'util';
+import mock from 'mock-fs';
 
-describe.only('Util - CreateFolder', () => {
+const readdir = promisify(fs.readdir);
+
+describe('Util - CreateFolder', () => {
+  afterEach(mock.restore);
+
   it('returns a string', async () => {
-    expect(await createFolder(), 'to be a string');
+    mock({});
+
+    await createFolder('hello-world');
+
+    const folderFiles = await readdir('./');
+
+    expect(folderFiles, 'to have length', 1);
+    expect(folderFiles[0], 'to equal', 'hello-world');
   });
 });
