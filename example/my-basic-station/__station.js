5728be1ebd8a2fe4f88f92d0d99972c9b264a30f
Michael Zoidl <github@michaelzoidl.com>
2020-07-05 06:48:58 +0200

    reset

--- example/my-basic-station/__station.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  requiredProps: ['content'],
-  run: props => ({
-    files: [{
-      filename: 'my-file.txt',
-      template: `My Content: ${props.content}`
-    }]
-  })
-};
